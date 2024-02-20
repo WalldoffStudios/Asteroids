6295af997d6a7e772b5c9fb8fd7189bc7ae6ed12
Walldoff Studios <Lethalbadger@hotmail.com>
2024-02-20 20:48:38 +0100

    Added movement check to input state

--- Assets/Asteroids/Code/Scripts/Player/PlayerInputState.cs
+++ Assets/Asteroids/Code/Scripts/Player/PlayerInputState.cs
@@ -7,6 +7,7 @@ namespace Asteroids
         public float HorizontalInput { get; set; }
         public float VerticalInput { get; set; }
         public Vector2 MovementInput => new Vector2(HorizontalInput, VerticalInput).normalized;
+        public bool IsMoving => MovementInput.sqrMagnitude > 0.0f;
         
         public bool IsFiring { get; set; }
     }   
