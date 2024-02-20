5d5ac883485cbdf6e23aa0d50d15fe14e51596a2
Walldoff Studios <Lethalbadger@hotmail.com>
2024-02-20 20:45:40 +0100

    Setup PlayerMoveHandler

--- Assets/Asteroids/Code/Scripts/Player/PlayerMoveHandler.cs
+++ Assets/Asteroids/Code/Scripts/Player/PlayerMoveHandler.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Zenject;
 
-public class PlayerMoveHandler : MonoBehaviour
+namespace Asteroids
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMoveHandler : IFixedTickable
     {
-        
-    }
+        [Serializable]
+        public class Settings
+        {
+            public float MoveSpeed;
+            public float MaxSpeed;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private readonly ScreenBorders _screenBorders;
+        private readonly Settings _settings;
+        private readonly Player _player;
+        private readonly PlayerInputState _inputState;
+
+        public PlayerMoveHandler(
+             ScreenBorders borders,
+            Player player, 
+            Settings settings, 
+            PlayerInputState inputState)
+        {
+            _screenBorders = borders;
+            _player = player;
+            _settings = settings;
+            _inputState = inputState;
+        }
+
+        public void FixedTick()
+        {
+            if(_player.IsDead == true) return;
+            
+            if (_inputState.IsMoving == true)
+            {
+                _player.AddForce(_inputState.MovementInput * _settings.MoveSpeed);
+            }
+            if (_player.Velocity.sqrMagnitude > 0.0f)
+            {
+                if (_screenBorders.IsNearEdge(_player.Position) == true)
+                {
+                    Vector2 direction = _screenBorders.GetBounceDirection(_player.Position, _player.Velocity);
+                    _player.Bounce(direction);
+                }
+            }
+        }
+    }   
 }
