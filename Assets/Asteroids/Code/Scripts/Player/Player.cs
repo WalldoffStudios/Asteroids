a566fae449b692a6440f9ab840dbfdc81ea73670
Walldoff Studios <Lethalbadger@hotmail.com>
2024-02-20 20:43:02 +0100

    Fixed Player being unintended monoBehaviour class

--- Assets/Asteroids/Code/Scripts/Player/Player.cs
+++ Assets/Asteroids/Code/Scripts/Player/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Asteroids
 {
-    public class Player : MonoBehaviour
+    public class Player
     {
         private readonly Rigidbody2D _rigidBody;
         public SpriteRenderer Renderer { get; }
@@ -16,7 +16,7 @@ namespace Asteroids
         private float _health;
         public bool IsDead { get; set; }
         public float Health { get; set; }
-        public Vector3 LookDir => -_rigidBody.transform.right;
+        public Vector2 LookDir => -_rigidBody.transform.right;
         
         public float Rotation
         {
@@ -24,19 +24,25 @@ namespace Asteroids
             set => _rigidBody.rotation = value;
         }
 
-        public Vector3 Position
+        public Vector2 Position
         {
             get => _rigidBody.position;
             set => _rigidBody.position = value;
         }
 
-        public Vector3 Velocity => _rigidBody.velocity;
+        public Vector2 Velocity => _rigidBody.velocity;
 
         public void TakeDamage(float healthLoss)
         {
             _health = Mathf.Max(0.0f, _health - healthLoss);
         }
 
-        public void AddForce(Vector2 force) => _rigidBody.AddForce(force);
+        public void Bounce(Vector2 direction) => _rigidBody.velocity = direction;
+
+        public void AddForce(Vector2 force)
+        {
+            _rigidBody.AddForce(force, ForceMode2D.Force);
+            _rigidBody.velocity = Vector2.ClampMagnitude(_rigidBody.velocity, 10.0f);
+        }
     }   
 }
