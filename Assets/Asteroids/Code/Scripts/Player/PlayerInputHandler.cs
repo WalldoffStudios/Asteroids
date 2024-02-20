0debd5f193e265904fb90892ba919de38a692080
Walldoff Studios <Lethalbadger@hotmail.com>
2024-02-20 22:57:50 +0100

    PlayerInputHandler now fires signalbus input events for movement

--- Assets/Asteroids/Code/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Asteroids/Code/Scripts/Player/PlayerInputHandler.cs
@@ -6,13 +6,30 @@ namespace Asteroids
     public class PlayerInputHandler : ITickable
     {
         private readonly PlayerInputState _inputState;
+        private readonly SignalBus _signalBus;
 
-        public PlayerInputHandler(PlayerInputState inputState) => _inputState = inputState;
-        
+        public PlayerInputHandler(PlayerInputState inputState, SignalBus signalBus)
+        {
+            _inputState = inputState;
+            _signalBus = signalBus;
+        }
+
         public void Tick()
         {
             _inputState.HorizontalInput = Input.GetAxisRaw("Horizontal");
             _inputState.VerticalInput = Input.GetAxisRaw("Vertical");
+            bool movedLastFrame = _inputState.LastMovementState;
+            bool currentlyMoving = _inputState.IsMoving;
+            if (movedLastFrame != currentlyMoving)
+            {
+                _signalBus.Fire(new MovementStateChangedSignal(currentlyMoving));
+                _inputState.LastMovementState = currentlyMoving;
+            }
+
+            if (currentlyMoving == true)
+            {
+                _signalBus.Fire(new MovementUpdateSignal(_inputState.MovementInput));
+            }
             
             _inputState.IsFiring = Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0);
         }
